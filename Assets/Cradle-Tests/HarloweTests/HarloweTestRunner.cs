178ef37674a5ac7d965995517436b06a9ba64eb5
D. A. Terre <doron.t@eyeway-vision.com>
2017-08-22 12:47:15 +0300

    Updated to 2017.1.0f3 and switched to UnityEngine.TestTools

---
+++ Assets/Cradle-Tests/HarloweTests/HarloweTestRunner.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System;
+using Cradle;
+using Cradle.StoryFormats.Harlowe;
+
+public class HarloweTestRunner: IPrebuildSetup
+{
+	void IPrebuildSetup.Setup()
+	{
+	}
+
+	class TestRunner: IDisposable
+	{
+		public readonly HarloweTests Story;
+
+		public TestRunner(string startPassage = null)
+		{
+			GameObject container = new GameObject();
+			container.name = "TestRunner: " + startPassage;
+			this.Story = container.AddComponent<HarloweTests>();
+			if (startPassage != null)
+				this.Story.StartPassage = startPassage;
+		}
+
+		public void Dispose()
+		{
+			GameObject.Destroy(this.Story.gameObject);
+		}
+	}
+
+	IEnumerator DoTest(string startPassage, Action<HarloweTests> test = null)
+	{
+		using (var testRunner = new TestRunner(startPassage))
+		{
+			yield return null;
+			if (test != null)
+				test(testRunner.Story);
+		}
+	}
+
+	[UnityTest]
+	public IEnumerator Markup_Links()
+	{
+		return DoTest("Markup_Links");
+	}
+
+	[UnityTest]
+	public IEnumerator Markup_Unsupported()
+	{
+		return DoTest("Markup_Unsupported");
+	}
+
+	[UnityTest]
+	public IEnumerator Markup_Verbatim()
+	{
+		return DoTest("Markup_Verbatim");
+	}
+
+	[UnityTest]
+	public IEnumerator Markup_LineBreaks()
+	{
+		return DoTest("Markup_LineBreaks");
+	}
+
+	[UnityTest]
+	public IEnumerator Markup_HTML()
+	{
+		return DoTest("Markup_HTML");
+	}
+
+	[UnityTest]
+	public IEnumerator Markup_Scope_Styles()
+	{
+		return DoTest("Markup_Scope_Styles");
+	}
+
+	[UnityTest]
+	public IEnumerator Markup_Scope_Hooks()
+	{
+		return DoTest("Markup_Scope_Hooks");
+	}
+
+	[UnityTest]
+	public IEnumerator Markup_NakedVar()
+	{
+		return DoTest("Markup_NakedVar");
+	}
+
+	[UnityTest]
+	public IEnumerator Vars_It()
+	{
+		return DoTest("Vars_It");
+	}
+
+	[UnityTest]
+	public IEnumerator Vars_Type_String()
+	{
+		return DoTest("Vars_Type_String");
+	}
+
+	[UnityTest]
+	public IEnumerator Vars_Type_Number()
+	{
+		return DoTest("Vars_Type_Number");
+	}
+
+	[UnityTest]
+	public IEnumerator Vars_Type_Bool()
+	{
+		return DoTest("Vars_Type_Bool");
+	}
+
+	[UnityTest]
+	public IEnumerator Vars_Type_Array()
+	{
+		return DoTest("Vars_Type_Array");
+	}
+
+	[UnityTest]
+	public IEnumerator Vars_Type_Dataset()
+	{
+		return DoTest("Vars_Type_Dataset");
+	}
+
+	[UnityTest]
+	public IEnumerator Vars_Type_Datamap()
+	{
+		return DoTest("Vars_Type_Datamap");
+	}
+}
--- Assets/Cradle-Tests/Tools/TestMacros.cs
+++ Assets/Cradle-Tests/Tools/TestMacros.cs
@@ -1,28 +1,25 @@
-﻿using UnityEngine;
-using Cradle;
-using UnityTest;
-using System.Collections;
-using System.Linq;
-using Cradle;
+﻿using Cradle;
 using Cradle.StoryFormats.Harlowe;
+using System.Linq;
+using NUnit.Framework;
 
 public class UnityTwineTestMacros: RuntimeMacros
 {
 	[RuntimeMacro]
 	public void assert(bool condition, string error)
 	{
-		IntegrationTest.Assert(condition, error);
+		Assert.IsTrue(condition, error);
 	}
 
 	[RuntimeMacro]
 	public void assertStyle(string setting, StoryVar value)
 	{
 		StoryStyle style = this.Story.GetCurrentStyle();
-		IntegrationTest.Assert(
+		Assert.IsTrue(
 			value == style[setting],
 			string.Format("Group's \"{0}\" style setting is not {1}", setting, value)
 		);
-		IntegrationTest.Assert(
+		Assert.IsTrue(
 			this.Story.Output.Reverse<StoryOutput>().Any(output => value == output.GetAppliedStyle()[setting]),
 			string.Format("The \"{0}\" style setting was not applied to any output", setting)
 		);
@@ -31,11 +28,11 @@ public class UnityTwineTestMacros: RuntimeMacros
 	[RuntimeMacro]
 	public void assertHook(string hookName)
 	{
-		IntegrationTest.Assert(
+		Assert.IsTrue(
 			this.Story.CurrentGroup.Style.Get<string>(HarloweStyleSettings.Hook) == hookName,
 			string.Format("The current group isn't defined as a Harlowe-hook named'{0}'.", hookName)
 		);
-		IntegrationTest.Assert(
+		Assert.IsTrue(
 			this.Story.Output.Reverse<StoryOutput>().Any(output => output.GetAppliedStyle().Get<string>(HarloweStyleSettings.Hook) == hookName),
 			string.Format("The Harlowe-hook '{0}' wasn't applied to any output, despite being in scope.", hookName)
 		);
@@ -44,24 +41,24 @@ public class UnityTwineTestMacros: RuntimeMacros
 	[RuntimeMacro]
 	public void assertText(string text, string error)
 	{
-		IntegrationTest.Assert(this.Story.GetCurrentText().Where(t => t.Text == text).Count() == 1, error);
+		Assert.IsTrue(this.Story.GetCurrentText().Where(t => t.Text == text).Count() == 1, error);
 	}
 
 	[RuntimeMacro]
 	public void assertLink(string text, string passage, string error = null)
 	{
-		IntegrationTest.Assert(this.Story.GetCurrentLinks().Where(link => link.Text == text && link.PassageName == passage).Count() == 1, error ?? text);
+		Assert.IsTrue(this.Story.GetCurrentLinks().Where(link => link.Text == text && link.PassageName == passage).Count() == 1, error ?? text);
 	}
 
 	[RuntimeMacro]
 	public void assertNoOutput(System.Type type )
 	{
-		IntegrationTest.Assert(this.Story.Output.Where(output => type.IsAssignableFrom(output.GetType())).Count() == 0);
+		Assert.IsTrue(this.Story.Output.Where(output => type.IsAssignableFrom(output.GetType())).Count() == 0);
 	}
 
 	[RuntimeMacro]
 	public void pass()
 	{
-		IntegrationTest.Pass();
+		//IntegrationTest.Pass();
 	}
 }
